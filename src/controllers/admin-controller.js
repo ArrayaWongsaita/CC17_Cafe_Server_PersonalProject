b3e83e25a2e4bebeac2493d73e6607b1d2afbae2
ArrayaWongsaita <wongsaita.a@gmail.com>
2024-06-08 14:37:15 +0700

    create admin api

--- src/controllers/admin-controller.js
+++ src/controllers/admin-controller.js
@@ -1,33 +1,73 @@
-const adminService = require("../services/admin-service")
-
-const adminController = {}
-
+const adminService = require("../services/admin-service");
+const fs = require("fs/promises");
+const createError = require("../utils/create-error");
+const uploadService = require("../services/upload-service");
+const removeService = require("../services/remove-service");
+const adminController = {};
 
 adminController.getAllOrder = async (req, res, next) => {
   try {
-    const allOrder = await adminService.getAllOrder()
-  res.status(200).json(allOrder)
+    const allOrder = await adminService.getAllOrder();
+    res.status(200).json(allOrder);
   } catch (error) {
-    next(error)
+    next(error);
   }
-}
+};
 adminController.getAllPendingOrder = async (req, res, next) => {
   try {
-    const allOrder = await adminService.getAllOrderPending()
-  res.status(200).json(allOrder)
+    const allOrder = await adminService.getAllOrderPending();
+    res.status(200).json(allOrder);
   } catch (error) {
-    next(error)
+    next(error);
   }
-}
+};
 adminController.editOrder = async (req, res, next) => {
   try {
-    
-  res.status(200).json({massage:"done"})
+  
+    if (
+      !req.body?.orderId ||
+      !(req.body?.address ||
+        req.body?.phone ||
+        req.body?.firstName ||
+        req.body?.lastName ||
+        req.file?.path ||
+        req.body?.price)
+    ){
+      createError({
+        message: "Incomplete information ",
+        statusCode: 400,
+      })
+    }
+    const data = req.body
+
+    if(data.price) data.price = +data.price
+    if(req?.file){
+      req.oldImage = await adminService.findSlipImageByOrderId(+data.orderId)
+
+      console.log(req.oldImage)
+      data.slipImage = await uploadService.upload(req.file?.path)
+    }
+    const orderId = +data.orderId
+    delete data.orderId 
+    console.log(data)
+    console.log(orderId)
+    const result = await adminService.editOrderByOrderId(orderId,data)
+
+
+      res.status(200).json(result);
   } catch (error) {
-    next(error)
+    next(error);
+  } finally {
+    if (req.file) {
+
+      fs.unlink(req.file.path);
+    }
+    if (req?.oldImage) {
+      const oldImage = req.oldImage.split("/");
+      const path = oldImage[oldImage.length - 1].split(".")[0];
+      console.log(removeService.remove(path));
+    }
   }
-}
+};
 
-
-
-module.exports = adminController
+module.exports = adminController;
--- src/services/admin-service.js
+++ src/services/admin-service.js
@@ -4,5 +4,15 @@ const adminService = {}
 
 adminService.getAllOrder = () => prisma.order.findMany({})
 adminService.getAllOrderPending = () => prisma.order.findMany({where: {status: "Pending"}})
+adminService.findSlipImageByOrderId =  async (id) =>{ 
+  const result = await prisma.product.findMany({
+  where: {id}
+})
+  return result[0]?.slipImage || false
+
+}
+
+adminService.editOrderByOrderId = (id,data) => prisma.order.update({where:{id},data})
+
 
 module.exports = adminService
